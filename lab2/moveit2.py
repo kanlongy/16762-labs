42e54c644b519788763f92ead87c7e048e2aa6db
kanlongy <yekanlong2000@outlook.com>
2026-02-13 12:48:56 -0500

    fix bugs

--- lab2/moveit2.py
+++ lab2/moveit2.py
@@ -21,7 +21,7 @@ class MoveMe(HelloNode):
             self.get_joint_pos('joint_wrist_yaw'), self.get_joint_pos('joint_wrist_pitch'), self.get_joint_pos('joint_wrist_roll')],
 
             #Pose {2}: 
-            [0.0, -0.6, -np.pi/2,
+            [0.0, -0.5, -np.pi/2,
             self.get_joint_pos('joint_lift'), 0.1, 0.1, 0.1, 0.1,
             self.get_joint_pos('joint_wrist_yaw'), self.get_joint_pos('joint_wrist_pitch'), self.get_joint_pos('joint_wrist_roll')],
 
