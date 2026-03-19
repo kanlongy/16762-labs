7e7d697098e2e5258dfafe286ea42840e10c0465
kanlongy <yekanlong2000@outlook.com>
2026-03-19 15:23:57 -0400

    add

--- lab4/stretch_navigation.py
+++ lab4/stretch_navigation.py
@@ -99,10 +99,10 @@ def main():
     # -----------------------------------------------------------------------
     waypoint_defs = [
         [0.0,  0.0,  0.0, 1.0  ],   # Pose 0 – starting pose
-        [1.7, -1.0, -0.8, 0.5  ],   # Pose 1 – facing +x
-        [1.6, -3.2, -0.7, 0.7  ],   # Pose 2 – facing +y  (~3 m from Pose 1)
-        [3.0, -4.7,  0.1, 1.0  ],   # Pose 3 – facing -x  (~3 m from Pose 2)
-        [3.8, -8.1, -0.7, 0.8  ],   # Pose 4 – facing -y  (~2.5 m from Pose 3)
+        [1.4, -1.0, -0.6, 0.8  ],   # Pose 1 – facing +x
+        # [1.6, -3.2, -0.7, 0.7  ],   # Pose 2 – facing +y  (~3 m from Pose 1)
+        # [3.0, -4.7,  0.1, 1.0  ],   # Pose 3 – facing -x  (~3 m from Pose 2)
+        # [3.8, -8.1, -0.7, 0.8  ],   # Pose 4 – facing -y  (~2.5 m from Pose 3)
     ]
 
     # ------ Set the initial pose (starting position in the map) ------
